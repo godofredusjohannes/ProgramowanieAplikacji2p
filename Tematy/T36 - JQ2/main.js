93c0855d011cae2e55346b6edf70493fee618d06
Seweryn <bobbyfngaming@gmail.com>
2025-02-28 15:10:19 +0100

    Aktualizacja

--- Tematy/T36 - JQ2/main.js
+++ Tematy/T36 - JQ2/main.js
@@ -1,5 +1,18 @@
 $(function() {
-    $('#bibl').click(function() {
-        $('#bibl_info').toggle("slow");
+        $('#zakladka_info2').toggle();
+        $('#zakladka_info3').toggle();
+        $('#zakladka_info4').toggle();
+    $('#zakladka1').click(function() {
+        $('#zakladka_info1').toggle("slow");
+    });
+    $('#zakladka2').click(function() {
+        $('#zakladka_info2').toggle("slow");
+    });
+    $('#zakladka3').click(function() {
+        $('#zakladka_info3').toggle("slow");
+    });
+    $('#zakladka4').click(function() {
+        $('#zakladka_info1').;
+        $('#zakladka_info4').toggle("slow");
     });
 });
