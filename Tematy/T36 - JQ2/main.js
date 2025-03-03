b204563f4a0dcade8cb270683f73f93846f3344c
Seweryn <bobbyfngaming@gmail.com>
2025-03-03 16:02:29 +0100

    Aktualizacja

--- Tematy/T36 - JQ2/main.js
+++ Tematy/T36 - JQ2/main.js
@@ -1,18 +1,14 @@
-$(function() {
-        $('#zakladka_info2').toggle();
-        $('#zakladka_info3').toggle();
-        $('#zakladka_info4').toggle();
-    $('#zakladka1').click(function() {
-        $('#zakladka_info1').toggle("slow");
-    });
-    $('#zakladka2').click(function() {
-        $('#zakladka_info2').toggle("slow");
-    });
-    $('#zakladka3').click(function() {
-        $('#zakladka_info3').toggle("slow");
-    });
-    $('#zakladka4').click(function() {
-        $('#zakladka_info1').;
-        $('#zakladka_info4').toggle("slow");
+$(document).ready(function () {
+    $("div[id^='zakladka_info']").hide();
+    $("#zakladka_info1").show();
+
+    $("p[id^='zakladka']").click(function () {
+        let id = $(this).attr("id").replace("zakladka", "zakladka_info");
+
+        if ($("#" + id).is(":visible")) return;
+
+        $("div[id^='zakladka_info']").hide();
+
+        $("#" + id).show();
     });
 });
---
+++ Tematy/T37 - JQ3/skrypt_c.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.thing').css({
+        'background-color': 'red',
+        'align-content': 'center',
+        'height': '350px',
+        'width': '350px',
+        'border-radius': '400px 0 0 0',
+        'margin': 'auto'
+    })
+
+    $('#box').css({
+        'background-color': 'yellow',
+        'align-content': 'center',
+        'height': '350px',
+        'width': '350px',
+        'margin': 'auto'
+    })
+    $('.thing').fadeOut(1000);
+    $('#box').show();
+
+
+});
